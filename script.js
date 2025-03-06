22c9fcc14f32f570c9c9aaf762d71f1e06de529c
sourav jain <95373772+souravgreatest@users.noreply.github.com>
2025-03-06 21:45:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 function makeid(l) {
   // write your code here
+	var res="";
+	var char_list="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+	for(var i=0;i<l;i++)
+		{
+			res+= char_list.charAt(
+				Math.floor(
+					Math.random()*chat_list.length
+				)
+			) 
+		}
+	return res;
 }
 
 // Do not change the code below.
